fa747c493536f5b6a0f1aad63ea90516b7486572
Ly <liraedata59@gmail.com>
2026-02-09 12:06:48 +0800

    feat: add gas sensor integration

--- src/main.cpp
+++ src/main.cpp
@@ -2,29 +2,43 @@
 
 const uint8_t FLAME_SENSOR_PINS[] = {9, 10, 11, 12, 13};
 
-boolean getFlameSensorData()
+const uint8_t GAS_SENSOR_DIGITAL_PIN = 8;
+const uint8_t GAS_SENSOR_ANALOG_PIN = A0;
+
+uint8_t getFlameSensorData()
 {
   for (uint8_t flameSensorPin : FLAME_SENSOR_PINS)
   {
     uint8_t sensorValue = digitalRead(flameSensorPin);
     if (sensorValue == LOW)
     {
-      return true; // Flame detected
+      return 1; // Flame detected
     }
   }
-  return false; // No flame detected
+  return 0; // No flame detected
 }
 
 void setup()
 {
+  Serial.begin(9600);
+
   for (uint8_t flameSensorPin : FLAME_SENSOR_PINS)
   {
     pinMode(flameSensorPin, INPUT_PULLUP);
   }
+
+  pinMode(GAS_SENSOR_DIGITAL_PIN, INPUT_PULLUP);
 }
 
 void loop()
 {
-  boolean flameDetected = getFlameSensorData();
-  Serial.println("Flame detected: " + String(flameDetected));
+  uint8_t flameDetected = getFlameSensorData();
+
+  uint8_t gasSensorDigitalValue = digitalRead(GAS_SENSOR_DIGITAL_PIN);
+
+  uint16_t gasSensorAnalogValue = analogRead(GAS_SENSOR_ANALOG_PIN);
+
+  Serial.println("flameDetected=" + String(flameDetected));
+  Serial.println("gasDetected=" + String(gasSensorDigitalValue));
+  Serial.println("gasSensorValue=" + String(gasSensorAnalogValue));
 }
