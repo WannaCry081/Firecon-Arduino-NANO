a8479443e49549eab5df2de7a908a98ac3127157
Ly <liraedata59@gmail.com>
2026-02-15 15:27:02 +0800

    feat: improve serial communication setup and data reporting format

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,71 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 
 #include "dht.hpp"
 #include "flame.hpp"
 #include "gas.hpp"
 
+unsigned long startTime = 0;
+
+uint8_t RX_PIN = 10,
+        TX_PIN = 11;
+
+SoftwareSerial esp(RX_PIN, TX_PIN); // RX, TX (Nano D10 = RX, D11 = TX)
+
 void setup()
 {
   // Initialize serial communication for debugging
-  Serial.begin(9600);
+  Serial.begin(115200);
+  esp.begin(9600);
 
+  Serial.println("Initializing flame sensors...");
   initFlameSensors();
+
+  Serial.println("Initializing Gas sensor...");
   initGasSensor();
+
+  Serial.println("Arduino NANO is ready.");
 }
 
 void loop()
 {
+  unsigned long currentMillis = millis();
+
   float temperature = 0.0f;
   float humidity = 0.0f;
 
-  uint8_t gasSensorDigitalValue = readGasSensorDigital();
-  uint16_t gasSensorAnalogValue = readGasSensorAnalog();
-
-  uint8_t flameDetected = getFlameSensorReading();
-  uint8_t hasDHTReading = getDHTSensorReading(&temperature, &humidity);
-
-  Serial.println("flameDetected=" + String(flameDetected));
-  Serial.println("gasDetected=" + String(gasSensorDigitalValue));
-  Serial.println("gasSensorValue=" + String(gasSensorAnalogValue));
-
-  if (hasDHTReading)
+  if (currentMillis - startTime >= 500)
   {
-    Serial.println("temperature=" + String(temperature));
-    Serial.println("humidity=" + String(humidity));
-  }
-  else
-  {
-    Serial.println("temperature=error");
-    Serial.println("humidity=error");
-  }
+    startTime = currentMillis;
 
-  delay(1000);
+    uint8_t gasSensorDigitalValue = readGasSensorDigital();
+    uint16_t gasSensorAnalogValue = readGasSensorAnalog();
+
+    uint8_t flameDetected = getFlameSensorReading();
+    uint8_t hasDHTReading = getDHTSensorReading(&temperature, &humidity);
+
+    esp.print("flameDetected=");
+    esp.println(flameDetected);
+    esp.print("gasDetected=");
+    esp.println(gasSensorDigitalValue);
+    esp.print("gasSensorValue=");
+    esp.println(gasSensorAnalogValue);
+
+    if (hasDHTReading)
+    {
+      esp.print("temperature=");
+      esp.println(temperature);
+      esp.print("humidity=");
+      esp.println(humidity);
+    }
+    else
+    {
+      esp.print("temperature=");
+      esp.println("error");
+      esp.print("humidity=");
+      esp.println("error");
+    }
+
+    esp.println("---");
+  }
 }
